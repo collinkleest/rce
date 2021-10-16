84965a099995b582335b191b25e1408d6434a1a4
Collin Kleest <collinkleest@gmail.com>
2021-10-15 23:35:47 -0400

    login / signup form updates

--- frontend/src/Components/Nav/Nav.tsx
+++ frontend/src/Components/Nav/Nav.tsx
@@ -139,8 +139,11 @@ export const Nav: React.FC<NavProps> = (props: NavProps) => {
           {loginIsOpen && loginViewActive && (
             <Form>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Label>Email or Username</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email or Username"
+                />
                 <Form.Text className="text-muted">
                   We'll never share your email with anyone else.
                 </Form.Text>
@@ -151,7 +154,7 @@ export const Nav: React.FC<NavProps> = (props: NavProps) => {
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+                <Form.Check type="checkbox" label="Keep me logged in" />
               </Form.Group>
               <Button variant="primary" type="submit">
                 Login
@@ -170,13 +173,16 @@ export const Nav: React.FC<NavProps> = (props: NavProps) => {
 
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control
+                  type="password"
+                  placeholder="Choose wisely, keep it secret"
+                />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password Confirmation</Form.Label>
                 <Form.Control
                   type="password"
-                  placeholder="Confirm your password"
+                  placeholder="Enter it again, don't mess up"
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="formBasicCheckbox">
